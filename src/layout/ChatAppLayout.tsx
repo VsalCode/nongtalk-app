72a89aa3a007c3115ad6f0c20e904f172b44d686
VsalCode <vsal.ilham@gmail.com>
2025-08-02 18:20:47 +0700

    fix(layout): re-adjusting chat app layout

--- src/layout/ChatAppLayout.tsx
+++ src/layout/ChatAppLayout.tsx
@@ -4,11 +4,18 @@ import HeaderBar from "../components/common/HeaderBar"
 
 const ChatAppLayout = () => {
   return (
-    <main className='bg-black flex items-center justify-center min-h-screen w-full'  >
-      <section className='text-white bg-gray w-full max-w-md min-h-screen flex flex-col items-center px-5 py-7 relative sm:min-h-[100vh] sm:max-h-screen sm:rounded-lg sm:my-4' >
-          <HeaderBar />
-          <Outlet/>
-          <TabBar/>
+    <main
+      className='flex items-center justify-center max-h-[100svh] w-full'
+      style={{
+        background: "radial-gradient(circle, rgba(88,76,214,1) 0%, rgba(20,20,20,1) 50%)",
+      }}
+    >
+      <section className='text-white bg-black w-full max-w-md flex flex-col items-center px-5 relative min-h-[100svh] max-h-[100svh] rounded-lg my-4' >
+        <HeaderBar />
+        <div className="flex-1 flex flex-col w-full pt-16 pb-20 overflow-hidden">
+          <Outlet />
+        </div>
+        <TabBar />
       </section>
     </main>
   )
