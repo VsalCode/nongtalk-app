cfd629cc3d37fb2ce49a62cd6be11bd194e63fd5
VsalCode <vsal.ilham@gmail.com>
2025-08-06 16:32:00 +0700

    feat(pages): integrate FriendListPage with backend api to display user logged in friend lists

--- src/pages/FriendsListPage.tsx
+++ src/pages/FriendsListPage.tsx
@@ -1,16 +1,51 @@
 import InputText from "../components/ui/InputText"
 import { IoSearch } from "react-icons/io5";
 import FriendCard from "../components/common/FriendCard";
+import React from "react";
+import { http } from "../utils/api/axios";
+import toast from "react-hot-toast";
+import { useAuth } from "../hooks/useAuth";
+import loadGIF from "../assets/loading.gif"
 
-const friends = [
-  { username: "Alice Johnson", usercode: "USR72381" },
-  { username: "Bob Smith", usercode: "USR72381" },
-  { username: "Charlie Brown", usercode: "USR72381" },
-  { username: "Diana Prince", usercode: "USR72381" },
-  { username: "Eve Wilson", usercode: "USR72381" }
-]
+interface FriendLists {
+  id: number
+  userCode: string
+  email: string
+  username: string
+  password: string
+  createdAt: string
+}
 
 const FriendsListPage = () => {
+  const [friends, setFriends] = React.useState<FriendLists[]>([])
+  const { token } = useAuth()
+  const [isLoading, setIsLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    async function fetchData() {
+      if (!token) return
+      try {
+        setIsLoading(true)
+        const res = await http(token as string).get("/friends")
+        setFriends(res.data.results)
+      } catch (err) {
+        const errMsg = err instanceof Error ? err.message : "unknown error"
+        toast.error(errMsg)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [token])
+
+  if (isLoading) {
+    return (
+      <main className="flex bg-black justify-center items-center min-h-screen">
+        <img src={loadGIF} alt="loading-gif" className="w-50" />
+      </main>
+    )
+  }
+
   return (
     <>
       <section className="mt-5 w-full flex flex-col gap-5 flex-1 overflow-hidden" >
@@ -32,7 +67,7 @@ const FriendsListPage = () => {
             <FriendCard
               key={index}
               username={e.username}
-              usercode={e.usercode}
+              usercode={e.userCode}
             />
           ))}
         </div>
